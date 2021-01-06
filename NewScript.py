656824cbf050c27c8f7268dbc16b92f00346195c
WortTuner <schnurdeniz62@gmail.com>
2021-01-06 19:47:11 +0100

    Update and rename WaSpam.py to NewScript.py

--- NewScript.py
+++ NewScript.py
@@ -1,8 +1,12 @@
+clear
 print("Erhalte Informationen von +49 174 8830551...")
 print("")
 print("Betreiber : VODAFONE")
-print("Standort : Storkow (Mark), Brandenburg")
+print("Standort : Storkow (Mark), Brandenburg, 15859")
 print("Akkumulator in % : 67")
 print("Handynummer registriert auf : Milano El Egit")
 print("Empfang : 2/4")
 print("Letzte geöffnete Applikation : Ameza")
+
+print("")
+print("Standort kann um 2 Kilometer abweichen!")
